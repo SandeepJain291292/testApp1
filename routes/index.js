ddc1707a5ed54f89a2eb1548bd357a8173241238
sandeep jain <sandeep.jain@fexle.com>
2021-03-17 17:06:24 +0530

    my app1

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.setHeader('set-cookie', [
+    'cookie1=value1; SameSite=Lax',
+    'cookie2=value2; SameSite=None; Secure',
+  ]);
   res.render('index', { title: 'App1' });
 });
 
