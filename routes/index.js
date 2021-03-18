0303682cfe259ec884207e2f8491cd96f848c267
sandeep jain <sandeep.jain@fexle.com>
2021-03-18 15:17:28 +0530

    my app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  req.session.i = 1;
+  // req.session.i = 1;
   // console.log('req.cookies[connect.sid]-----------'+req.session.i);
   // console.log('req.cookies[connect.sid]-----------'+req.cookies['connect.sid']);
   // cookies.defaults.secure = true;
