adaa165a328ec23b40d91d39167f20b4ea44886a
sandeep jain <sandeep.jain@fexle.com>
2021-03-18 14:47:59 +0530

    my app1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,19 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({ 
+  secret: 'secret', 
+  resave: true, 
+  saveUninitialized: true, 
+  httpOnly:false, 
+  cookie: {
+    maxAge: 3600000000000,
+    httpOnly: false,
+    secure: true,
+    SameSite: 'None'
+  }
+})); //Session setup
 
 app.use(logger('dev'));
 app.use(express.json());
