584c776a7743665edb2d3a60249208a0be3d3a98
sandeep jain <sandeep.jain@fexle.com>
2021-03-18 14:45:24 +0530

    my app1

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.setHeader('set-cookie', [
-    'cookie1=value1; SameSite=Lax',
-    'cookie2=value2; SameSite=None; Secure',
-  ]);
+  req.session.i = 1;
+  console.log('req.cookies[connect.sid]-----------'+req.session.i);
+  console.log('req.cookies[connect.sid]-----------'+req.cookies['connect.sid']);
+  // cookies.defaults.secure = true;
+  // cookies.defaults.SameSite = 'None';
+  // res.setHeader('set-cookie', [
+  //   `connect.sid=${req.sessionID}; SameSite=None: Secure`
+  // ]);
   res.render('index', { title: 'App1' });
 });
 
